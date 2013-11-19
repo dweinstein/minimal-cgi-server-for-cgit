1605444f5a59f73aae296887781d2b832d733596
David Weinstein <dweinst@insitusec.com>
2013-11-19 17:48:48 +0000

    port back to python 2.x

---
+++ server-py2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import os
+
+from CGIHTTPServer import CGIHTTPRequestHandler
+from BaseHTTPServer import HTTPServer
+
+os.environ['CGIT_CONFIG'] = 'cgitrc' # in current working directory
+
+class RequestHandler(CGIHTTPRequestHandler):
+    def is_cgi(self):
+        cgi = '/cgit.cgi'
+        self.cgi_info = cgi, self.path[len(cgi):] # setting the PATH_INFO correctly eliminates the need of URL rewriting
+        return self.path == '/' or self.path.startswith(cgi)
+
+port = 8000
+httpd = HTTPServer(('127.0.0.1', port), RequestHandler)
+
+print 'start serving on port %d...' % port
+
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+
