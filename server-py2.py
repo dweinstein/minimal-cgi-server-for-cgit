bee36dfe0bbfd6f66f3027a0de4261f117ec9e08
David Weinstein <dweinst@insitusec.com>
2013-11-19 17:52:00 +0000

    listen on all interfaces.

--- server-py2.py
+++ server-py2.py
@@ -9,12 +9,12 @@ os.environ['CGIT_CONFIG'] = 'cgitrc' # in current working directory
 
 class RequestHandler(CGIHTTPRequestHandler):
     def is_cgi(self):
-        cgi = '/cgit.cgi'
+        cgi = '/cgit.cgi.linux_x86-64'
         self.cgi_info = cgi, self.path[len(cgi):] # setting the PATH_INFO correctly eliminates the need of URL rewriting
         return self.path == '/' or self.path.startswith(cgi)
 
 port = 8000
-httpd = HTTPServer(('127.0.0.1', port), RequestHandler)
+httpd = HTTPServer(('', port), RequestHandler)
 
 print 'start serving on port %d...' % port
 
